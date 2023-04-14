6f4f83484d0dac1a9f8dfc82078744077dfb0bac
DMN <dimos@mail.ru>
2023-04-14 13:02:46 +0300

    Zadacha8

---
+++ Zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int N = int.Parse(userInput);
+for (int i = 1; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine($" {i} ");
+    }
+}
