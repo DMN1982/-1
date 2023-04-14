11960c92dfdc63e76de7717b61cd96f7803e051a
DMN <dimos@mail.ru>
2023-04-14 10:35:45 +0300

    Zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число 1: ");
+string userInput1 = Console.ReadLine() ?? "";
+int usernumber1 = int.Parse(userInput1);
+Console.Write("Введите число 2: ");
+string userInput2 = Console.ReadLine() ?? "";
+int usernumber2 = int.Parse(userInput2);
+if (usernumber1 > usernumber2)
+{
+    Console.WriteLine(usernumber1);
+}
+else
+{
+    Console.WriteLine(usernumber2);
+}
